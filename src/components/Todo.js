4dc0fff25a8695fea37e902b404cbdd02a7f63d4
Shital Unde <shital.unde@gtcsys.com>
2022-07-13 11:28:51 +0530

    initial commit for react.js

---
+++ src/components/Todo.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+
+function Todo(props) {
+    const [modalIsOpen, setModelIsOpen] = useState(false);
+
+    function deleteHandler(e) {
+        console.log("Hello", e);
+        setModelIsOpen(true);
+    }
+
+    function closeModalHandler() {
+        setModelIsOpen(false);
+
+    }
+    console.log(modalIsOpen, "*****************");
+    return (
+        <div className="card">
+            <h2>{props.text}</h2>
+            <div className="actions">
+                <button onClick={(e) => deleteHandler(props.text)} className="btn">Delete</button>
+            </div>
+            {/* { showModal ? <Modal/> : null} */}
+            {modalIsOpen && <Modal />}
+            {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
+
+        </div>
+    );
+}
+
+export default Todo
