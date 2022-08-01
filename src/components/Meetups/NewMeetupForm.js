f6dff47d1af022fffffe76d501d00255521ec70e
Shital Unde <shital.unde@gtcsys.com>
2022-08-01 12:23:11 +0530

    form creation

--- src/components/Meetups/NewMeetupForm.js
+++ src/components/Meetups/NewMeetupForm.js
@@ -3,21 +3,49 @@ import Card from "../ui/Card"
  
 
 function NewMeetupForm() {
-    useRef()
-    function submitHandler () {
-        console.log("sumit")
+    const titleInputRef = useRef()
+    const addressInputRef = useRef()
+
+
+    function submitHandler (event) {
+        event.preventDefault()
+
+        const enteredTitle = titleInputRef.current.value
+        const enteredAddress= addressInputRef.current.value
+
+        const meetupData = {
+                userId:7,
+                date:"2020-02-03",
+                products:[
+                    {productId:5,quantity:1},
+                    {productId:1,quantity:5}
+                ]  
+        }
+       // console.log(meetupData)
+
+        // fetch("https://fakestoreapi.com/carts",
+        //     {
+        //         method: "POST",
+        //         cache: "no-cache",
+        //         headers:{
+        //             Accept: "application/json",
+        //             "Content-Type": "application/json",
+        //         },
+        //         body: JSON.stringify(meetupData)
+        //     })
+        //     .then(response=> response.json())
     }
     return (
         <Card>
             <form onSubmit={submitHandler}>
                 <div>
                     <label htmlFor="title">Title</label>
-                    <input type='text'  required id="title" />
+                    <input type='text' ref={titleInputRef} required id="title" />
                 </div>
                 <br/>
                 <div>
                     <label htmlFor="address">Address</label>
-                    <input type='text' required id="address" />
+                    <input type='text' ref={addressInputRef} required id="address" />
                 </div>
                 <div>
                     <button> Add Meetup</button>
